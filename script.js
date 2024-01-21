5c5541b0cbd70c31e9fdf94918f2b8839a3110fa
AngjelovDev <156189917+AngjelovDev@users.noreply.github.com>
2024-01-21 11:39:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
     copecartUpsell.run();
     }, false);
+
+    function getCurrentDay() {
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var today = new Date();
+        var dayNumber = today.getDay();
+        var dayName = days[dayNumber];
+         
+        document.getElementById('currentDay').textContent = dayName;
+       }
+       
+       getCurrentDay();
